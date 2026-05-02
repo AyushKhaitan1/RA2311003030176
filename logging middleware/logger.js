55ef2f10d93c2ef21c810e27a45a105dc9aa54d5
Ayush Khaitan <ayushkhaitan2004@gmail.com>
2026-05-02 11:04:51 +0530

    feat: add mandatory logging middleware

---
+++ logging middleware/logger.js
@@ -0,0 +1,31 @@
+// logging middleware/logger.js
+
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhazk0MzBAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzY5OTY5OSwiaWF0IjoxNzc3Njk4Nzk5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiYjE0ODMyNzUtNmQ3OC00ZDE2LWExZDMtZGIxYjdmZTlkYTk2IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoiYXl1c2gga2hhaXRhbiIsInN1YiI6IjE0ZGMxZWUzLTA5MzItNDdlMC1iMWVhLTM2NWQ1OTA3ZjM3NyJ9LCJlbWFpbCI6ImFrOTQzMEBzcm1pc3QuZWR1LmluIiwibmFtZSI6ImF5dXNoIGtoYWl0YW4iLCJyb2xsTm8iOiJyYTIzMTEwMDMwMzAxNzYiLCJhY2Nlc3NDb2RlIjoiUWticHhIIiwiY2xpZW50SUQiOiIxNGRjMWVlMy0wOTMyLTQ3ZTAtYjFlYS0zNjVkNTkwN2YzNzciLCJjbGllbnRTZWNyZXQiOiJjUmZNTkt1Qnp0RlNzQmZQIn0.YEMWK0NeU11rjuUaK9vjuwwWEPVXjkAeqJVCSg6vobI"; 
+
+async function Log(stack, level, pkg, message) {
+    try {
+        const response = await fetch("http://20.207.122.201/evaluation-service/logs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${AUTH_TOKEN}`
+            },
+            body: JSON.stringify({
+                stack: stack,       
+                level: level,       
+                package: pkg,       
+                message: message
+            })
+        });
+
+        if (!response.ok) {
+            console.error("Logger Failed:", await response.text());
+        } else {
+            console.log(`[Logged] ${stack} -> ${pkg}: ${message}`);
+        }
+    } catch (error) {
+        console.error("Error connecting to logger:", error.message);
+    }
+}
+
+export default Log;
